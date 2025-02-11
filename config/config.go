df47a42e6de884445e3caf6ec46735149d964877
Rani Bungay <ranibungay@gmail.com>
2025-02-11 12:40:16 -0800

    refactor config for development

--- config/config.go
+++ config/config.go
@@ -7,18 +7,21 @@ import (
 	"github.com/joho/godotenv"
 )
 
+
 func LoadConfig() {
 	err := godotenv.Load()
-	if err != nil {
-		log.Println("Warning: No .env file found, using system env variables")
-	} else {
-		log.Println(".env file loaded successfully.")
-	}
+	handleConfigError(err, "Warning: No .env file found, using system env variables.")
 
-	log.Println("RUNSIGNUP_API_URL:", GetEnv("RUNSIGNUP_API_URL", "NOT SET"))
-	log.Println("RUNSIGNUP_API_KEY:", GetEnv("RUNSIGNUP_API_KEY", "NOT SET"))
-	log.Println("RUNSIGNUP_API_SECRET:", GetEnv("RUNSIGNUP_API_SECRET", "NOT SET"))
+	log.Println(".env file loaded successfully.")
+
+
+	if os.Getenv("ENV") == "development" {
+		log.Println("RUNSIGNUP_API_URL:", GetEnv("RUNSIGNUP_API_URL", "NOT SET"))
+		log.Println("RUNSIGNUP_API_KEY:", GetEnv("RUNSIGNUP_API_KEY", "NOT SET"))
+		log.Println("RUNSIGNUP_API_SECRET:", GetEnv("RUNSIGNUP_API_SECRET", "NOT SET"))
+	}
 }
+
 
 func GetEnv(key, fallback string) string {
 	value, exists := os.LookupEnv(key)
@@ -26,4 +29,11 @@ func GetEnv(key, fallback string) string {
 		return fallback
 	}
 	return value
+}
+
+
+func handleConfigError(err error, message string) {
+	if err != nil {
+		log.Println(message)
+	}
 }
