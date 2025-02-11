722e55c66494e964bf7e6ef3b224efd11c645809
Rani Bungay <ranibungay@gmail.com>
2025-02-11 14:10:54 -0800

    separate models and constants from service logic

---
+++ internal/api/runsignup/models/events.go
@@ -0,0 +1,13 @@
+package models
+
+
+type Event struct {
+	ID        int    `json:"race_id"`
+	Name      string `json:"name"`
+	StartDate string `json:"next_date"`
+	EndDate   string `json:"next_end_date"`
+	URL       string `json:"url"`
+	City      string `json:"city"`
+	State     string `json:"state"`
+	Zipcode   string `json:"zipcode"`
+}
