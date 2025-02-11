6e520b1f6bd2faaa6b6db17134d5397cbfeca736
Rani Bungay <ranibungay@gmail.com>
2025-02-11 12:19:29 -0800

    create fetch service

--- config/config.go
+++ config/config.go
@@ -11,7 +11,13 @@ func LoadConfig() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("Warning: No .env file found, using system env variables")
+	} else {
+		log.Println(".env file loaded successfully.")
 	}
+
+	log.Println("RUNSIGNUP_API_URL:", GetEnv("RUNSIGNUP_API_URL", "NOT SET"))
+	log.Println("RUNSIGNUP_API_KEY:", GetEnv("RUNSIGNUP_API_KEY", "NOT SET"))
+	log.Println("RUNSIGNUP_API_SECRET:", GetEnv("RUNSIGNUP_API_SECRET", "NOT SET"))
 }
 
 func GetEnv(key, fallback string) string {
--- internal/api/runsignup/services/fetch_events_services.go
+++ internal/api/runsignup/services/fetch_events_services.go
@@ -1,1 +1,11 @@
 package services
+
+import (
+	"fmt"
+)
+
+
+func FetchEventsByState(state string) (string, error) {
+	fmt.Println("Fetching events for state:", state)
+	return "Event list for " + state, nil
+}
